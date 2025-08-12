8d701d45e6dea92c295c2de011d8ecf3cf00e0f9
Natanbmoura <natan.bmoura1@gmail.com>
2025-08-12 16:41:34 -0300

    primeiro commit

---
+++ Index.js
@@ -0,0 +1,18 @@
+let nota1=8.3
+let nota2=5
+let nota3=3
+let nota4=1
+let media= (nota1+nota2+nota3+nota4)/4
+console.log(media)
+
+let numero = Math.sqrt(9)
+
+console.log(numero)
+ let a
+ let b
+ let c
+ let delta = ((b*b)-4*a*c)
+ let resposta = Math.sqrt(delta)
+ resultado = (-b + resposta)/(2*a)
+
+ console.log ("o resultado é", resultado)
