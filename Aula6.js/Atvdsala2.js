77ff70d4b9fdd8cf2ebc35880ca13e51d87009f5
deborakesia <deboracavalcanti8441@gmail.com>
2025-08-21 16:51:21 -0300

    atvdaula6

--- Aula6.js/Atvdsala2.js
+++ Aula6.js/Atvdsala2.js
@@ -1,24 +1,27 @@
 let readline = require('readline-sync');
 
-let nmr1 = Number( readline.question ("digite o primeiro numero"))
-let nmr2 = Number( readline.question ("digite o segundo numero"))
+let nmr1 = Number( readline.question ("digite o primeiro numero: "))
+let nmr2 = Number( readline.question ("digite o segundo numero: "))
 
-console.log("opção1 soma")
-console.log("opção2 divisão")
-console.log("opção3 multiplicação")
-console.log("opção4 subtração")
+console.log("opção 1 soma")
+console.log("opção 2 divisão")
+console.log("opção 3 multiplicação")
+console.log("opção 4 subtração")
 
-let opcao = readline.question ("digite uma das opções")
+let opcao = readline.question ("digite uma das opções: ")
 switch (opcao){
-    case "opção1": 
+    case "1": 
     console.log ("o seu resultado foi: ", nmr1 + nmr2)
     break;
-    case "opção2":
+    case "2":
     console.log ("o seu resultado foi:", nmr1 / nmr2)
     break;
-    case "opção3":("o seu resoltado foi:", nmr1 * nmr2)
+    case "3":
+    console.log ("o seu resoltado foi:", nmr1 * nmr2)
     break;
-    case "opção4": ("o seu resultado foi:" , nmr1 - nmr2)
+    case "4":
+    console.log  ("o seu resultado foi:" , nmr1 - nmr2)
+    break;
 }
 
 
